f1fce94e21d66db19ebdb1038d34a633e3a93fa9
katm201 <katm201@gmail.com>
2017-10-20 15:24:00 -0700

    Installed bower & updated some app syntax.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,62 +5,63 @@
 
 
 $(document).ready(function () {
-  var $chats = $('#chats')
+  var $chats = $('#chats');
+  //maybe move this <div> creation over to the html?
   var $msgContainer = $('<div></div>');
-  $msgContainer.addClass('msgContainer')
-    $msgContainer.appendTo($chats);
-    
-    var message = {
-        username: null,
-        text: null,
-        roomname: null
-      };
-      $.ajax({
-        // This is the url you should use to communicate with the parse API server.
-        url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-        type: 'POST',
-        data: JSON.stringify(message),
-        contentType: 'application/json',
-        success: function (data) {
-          console.log('chatterbox: Message sent');
-        },
-        error: function (data) {
-          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-          console.error('chatterbox: Failed to send message', data);
-        }
-      });
+  $msgContainer.addClass('msgContainer');
+  $msgContainer.appendTo($chats);
+  
+  var message = {
+    username: 'user',
+    text: 'hey',
+    roomname: 'testRoom'
+  };
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent', data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
 
 
 
-    $.ajax({
-        // This is the url you should use to communicate with the parse API server.
-        url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-        type: 'GET',
-        success: function (data) {
-          console.log(data.results);
-          var messages = data.results
-          for (var i = 0; i < messages.length; i++) {
-            var userName = messages[i].username
-            var roomName = messages[i].roomname
-            var text = messages[i].text
-            var createdTime = messages[i].createdAt
+  $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    success: function (data) {
+      console.log(data);
+      var messages = data.results;
+      for (var i = 0; i < messages.length; i++) {
+        var userName = messages[i].username;
+        var roomName = messages[i].roomname;
+        var text = messages[i].text;
+        var createdTime = messages[i].createdAt;
 
-            var $message = $('<div></div>')
-            $message.addClass("message")
-            
-            $message.text(`username: ${userName} 
-            roomname: ${roomName} 
-            message:'${text}' 
-            created: ${createdTime}`)
-            
-            $message.appendTo($msgContainer);
-          }
-        },
-        error: function (data) {
-          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-          console.error('chatterbox: Failed to send message', data);
-        }
-      });
+        var $message = $('<div></div>');
+        $message.addClass('message');
+        
+        $message.text(`username: ${userName} 
+        roomname: ${roomName} 
+        message:'${text}' 
+        created: ${createdTime}`);
+        
+        $message.appendTo($msgContainer);
+      }
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
 
 });
 
