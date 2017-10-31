7ef36cb1ee715b4de5ad10251fe8fd1f7efc76e8
Hack Reactor Student <student@hackreactor.com>
2017-10-31 11:16:39 -0700

    Updated client to remove config and api key.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,7 +3,7 @@ var app = {
 
   //TODO: The current 'handleUsernameClick' function just toggles the class 'friend'
   //to all messages sent by the user
-  server: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+  server: 'http://127.0.0.1:3000/classes/messages',
   username: 'anonymous',
   roomname: 'lobby',
   lastMessageId: 0,
@@ -65,7 +65,7 @@ var app = {
       success: function(data) {
         // Don't bother if we have nothing to work with
         if (!data.results || !data.results.length) { return; }
-
+        // data = JSON.parse(data);
         // Store messages for caching later
         app.messages = data.results;
 
